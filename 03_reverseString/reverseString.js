f068f95e4df302a65789d671026b98afce21516e
rakurakuraku <127064071+rakurakuraku@users.noreply.github.com>
2023-06-30 10:52:40 +0800

    Add solutions for assignemnts 3 and 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let revs;
+    let arr = string.split("");
+    arr.reverse();
+    //for (char of arr) {
+    //    revs += `${char}`;
+    //}
+    revs = arr.join("");
+    return revs;
+
+
 
 };
 
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,25 @@
-const removeFromArray = function() {
+/*const removeFromArray = function(arr, ...rem) {
+    let array = arr;
+    let i = arr.indexOf(rem);
+    if (array.includes(rem)) {
+        array.splice(i, 1);
+        return array;
+
+}
+};
+*/
+const removeFromArray = function(arr, ...rem) {
+    let array = arr;
+      for (let r of rem) {
+        if (array.includes(r)) {
+          let i = array.indexOf(r);
+          array.splice(i, 1);
+        }
+      }
+      return array;
 
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
