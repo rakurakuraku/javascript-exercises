09bda898f1058ba83609d4fe854a2b48913d535d
rakurakuraku <127064071+rakurakuraku@users.noreply.github.com>
2023-09-13 15:41:31 +0800

    Add solution for JS exercise 5 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    let total = 0;
+    let big = 0;
+    let small = 0;
+    if (Number.isInteger(x) && Number.isInteger(y) && x > 0 && y > 0) {
+      if (x > y) {
+        big = x;
+        small = y;
+      }
+      else {
+        big = y;
+        small = x;
+      }
+      while (big >= small) {
+        total += big;
+        big--;
+        //console.log(big);
+        //console.log(total);
+      }
+      return(total);
+    }
+    else {
+      return('ERROR')
+    }
 };
 
 // Do not edit below this line
