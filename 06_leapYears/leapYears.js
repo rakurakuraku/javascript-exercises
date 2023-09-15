c6719b9b4f2d7457ffd84b454dbb58f5abf96c63
rakurakuraku <127064071+rakurakuraku@users.noreply.github.com>
2023-09-15 19:32:47 +0800

    Add solution for 06leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,13 @@
-const leapYears = function() {
-
-};
+const leapYears = function(year) {
+    let leapyear;
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+      leapyear = true;
+    }
+    else {
+      leapyear = false;
+    }
+    return(leapyear);
+}
 
 // Do not edit below this line
 module.exports = leapYears;
