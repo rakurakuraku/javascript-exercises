43e6b3e8ec7b93945ca2196c98db6b67e2a544c1
rakurakuraku <127064071+rakurakuraku@users.noreply.github.com>
2023-09-21 11:34:56 +0800

    Add solution to 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let result = (tempF -32) / 1.8;
+  let cResult = Number(result.toFixed(1));
+  return cResult;
 };
-
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let result = (tempC * 1.8) + 32;
+  let fResult = Number(result.toFixed(1));
+  return fResult;
 };
 
 // Do not edit below this line
