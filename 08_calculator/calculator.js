3fd23e3594ecf2cbe0c821e858d0f8fde4371ba9
rakurakuraku <127064071+rakurakuraku@users.noreply.github.com>
2023-09-22 14:36:00 +0800

    Add solutions for multiply, power and factorial in 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,20 +6,33 @@ const subtract = function(x, y) {
 	return(x - y);
 };
 
-const sum = function() {
-	
+const sum = function(x) {
+	let result = 0
+  for (let n in x) {
+    result += x[n];
+  }
+  return result;
 };
 
-const multiply = function() {
+const multiply = function(...nums) {
+  let result = 1;
+  for (let num of nums) {
+    result *= num;
+  }
+  return(result);
+}
 
+const power = function(num, pow) {
+	return(num ** pow);
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(numb) {
+	let result = 1;
+  while (numb >= 2) {
+    result *= numb;
+    --numb;
+  }
+  return(result);
 };
 
 // Do not edit below this line
