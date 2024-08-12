770a205eb576ca50d18d3c7a083a1701eec14300
rakurakuraku <127064071+rakurakuraku@users.noreply.github.com>
2024-08-12 22:09:12 +0800

    Add solution for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let newStr = '';
+    let revStr = '';
+    for (let i = 0; i < str.length; i++) {
+        if (!/^[^a-zA-Z0-9]$/.test(str[i]))
+          {newStr += str[i]};
+    };
+    for (let i = str.length -1; i >= 0; i --) {
+      if (!/^[^a-zA-Z0-9]$/.test(str[i])) 
+          {revStr += str[i]};
+    };
+    
+    newStr = newStr.toLowerCase();
+    revStr = revStr.toLowerCase();
 
+    if (newStr === revStr) {
+      return true;
+    } else { 
+      return false
+    };
 };
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
