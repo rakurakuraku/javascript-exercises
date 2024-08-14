0d5574a69d84612402d4614ece2f994e8cdbb230
rakurakuraku <127064071+rakurakuraku@users.noreply.github.com>
2024-08-14 15:50:09 +0800

    Add solution for fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let seed = [0, 1];
+    let i = 0;
+    if (num > 0) {
+      while (i < num -1) {
+        seed[seed.length] = seed[seed.length -1] + seed[seed.length -2];
+        i++;
+      };
+      return seed[num];
+    }
+      else {
+        return "OOPS";
+      }
 };
 
 // Do not edit below this line
