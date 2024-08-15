d403883fdabc066af0537ca365ed67d059aa9677
rakurakuraku <127064071+rakurakuraku@users.noreply.github.com>
2024-08-15 16:59:06 +0800

    Add solution for findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let thisYear = (new Date()).getFullYear();
 
+    for (let person = 0; person < arr.length; person ++) {
+      if (!('yearOfDeath' in arr[person])) {
+        arr[person].yearOfDeath = thisYear;
+      };
+    };
+
+    let sortedByAge = arr.sort(function(a, b) {return (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth)});
+
+    return sortedByAge[0];
 };
 
 // Do not edit below this line
